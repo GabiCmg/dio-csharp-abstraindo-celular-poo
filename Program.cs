880ce567d9b2b43a1e00090e1b395a6ba5af30bb
Gabriela Camargo <101142324+GabiCmg@users.noreply.github.com>
2024-06-02 15:33:21 -0300

    Abstraindo celular com POO - finalização

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using DesafioPOO.Models;
+
+// TODO: Realizar os testes com as classes Nokia e Iphone
+
+Console.WriteLine("\nSmartphone Iphone:");
+Smartphone iphone = new Iphone(numero: "1195252-7292", modelo: "Iphone 15 Pro Max", imei: "123456", memoria: 13);
+iphone.Ligar();
+iphone.InstalarAplicativo("WhatsApp");
+
+Console.WriteLine("\nSmartphone Nokia:");
+Smartphone nokia = new Nokia(numero: "1194575-8255", modelo: "Nokia X30", imei: "654321", memoria: 3);
+nokia.Ligar();
+nokia.InstalarAplicativo("Snake Game");
